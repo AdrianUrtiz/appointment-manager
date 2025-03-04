c0fcb2e8dc3a6f62921c08fde4a9699e5d3cc182
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2025-03-03 23:48:10 -0600

    Agregar componente Footer a las páginas About y Login; actualizar texto de copyright en Footer

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,20 @@
 'use client'
 
+import Footer from '@/components/partials/Footer'
+
 function page() {
   return (
-    <div className='min-h-screen w-screen flex flex-col items-center justify-center'>
-      <h1>About</h1>
-      <p>This is the about page</p>
-      <p>
-        You can use this page to tell your users a little bit about your app
-      </p>
-      <p>You can also use this page to document your app</p>
-    </div>
+    <>
+      <div className='min-h-screen w-full flex flex-col items-center justify-center'>
+        <h1>About</h1>
+        <p>This is the about page</p>
+        <p>
+          You can use this page to tell your users a little bit about your app
+        </p>
+        <p>You can also use this page to document your app</p>
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
   const texts = useTexts()
 
   return (
-    <div class='max-w-[90%] mx-auto items-centers grid grid-cols-1 justify-between gap-4 border-t border-gray-100 py-6 md:grid-cols-2'>
-      <p class='text-sm/6 text-slate-600 max-md:text-center'>
-        {texts.footer.copyright}
+    <div className='max-w-full md:px-24 mx-auto items-centers grid grid-cols-1 justify-between gap-4 border-t border-gray-100 py-6 md:grid-cols-2'>
+      <p className='text-sm/6 text-slate-600 max-md:text-center'>
+        © {new Date().getFullYear()} {texts.footer.copyright}
       </p>
-      <div class='flex items-center justify-center space-x-4 text-sm/6 font-semibold text-slate-900 md:justify-end'>
+      <div className='flex items-center justify-center space-x-4 text-sm/6 font-semibold text-slate-900 md:justify-end'>
         <a href=''>{texts.footer.privacy}</a>
-        <div class='h-4 w-px bg-slate-200'></div>
+        <div className='h-4 w-px bg-slate-200'></div>
         <a href=''>{texts.footer.terms}</a>
       </div>
     </div>
